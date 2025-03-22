0b6c782e28e1b156490408639d58049e40f75377
tlacuilose <tlacuilose@outlook.com>
2025-03-22 17:45:28 -0600

    Starts project

---
+++ Aite/AiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiteApp.swift
+//  Aite
+//
+//  Created by Jose Tlacuilo on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiteTests/AiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiteTests.swift
+//  AiteTests
+//
+//  Created by Jose Tlacuilo on 22/03/25.
+//
+
+import Testing
+@testable import Aite
+
+struct AiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
